fea7e8a8033632f17b2a400917bf887fa4092538
garciart <rgarcia@rgprogramming.com>
2019-11-20 11:16:24 -0500

    Corrected local checkout and duplicate folder issues.

--- CodingTemplates/CPP/hello.cpp
+++ CodingTemplates/CPP/hello.cpp
@@ -1,26 +1,11 @@
 /**
- * @brief Implementation of all C++ reserved keywords:
- * 
- * and, and_eq, asm, auto, bitand, bitor, bool, break, case, catch, char,
- * class, compl, const, const_cast, continue, default, delete, do, double,
- * dynamic_cast, else, enum, explicit, export, extern, false, float, for,
- * friend, goto, if, inline, int, long, mutable, namespace, new, not, not_eq,
- * operator, or, or_eq, private, protected, public, register, reinterpret_cast,
- * return, short, signed, sizeof, static, static_cast, struct, switch,
- * template, this, throw, true, try, typedef, typeid, typename, union,
- * unsigned, using, virtual, void, volatile, wchar_t, while, xor, and xor_eq
- * 
- * Styling using the Google C++ Style Guide at
- * https://google.github.io/styleguide/cppguide.html
- * 
- * @author  Rob Garcia
+ * @brief Template for C++ language coding. Styling using the Google C++ Style Guide at https://google.github.io/styleguide/cppguide.html.
+ * @author  Copyright 2019 Rob Garcia at rgarcia@rgprogramming.com.
  * @version 1.0
- * @see https://en.cppreference.com/w/cpp/keyword
  * @since   2019-02-10
+ * @see     https://en.cppreference.com/w/cpp/keyword
  * 
  * The MIT License
- *
- * Copyright 2019 Rob Garcia at rgarcia@rgprogramming.com.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -41,15 +26,22 @@
  * THE SOFTWARE.
  */
 
+/* ========== Include files and other header files ========== */
+
 #include <iostream>
 
-/**
- * To identify what functions are used by the standard namespace, we will preface each one with "std::",
- * instead of implementing the preprocessor directive "using namespace std;"
- */
+/* ========== Constants, #define, and enum statements ========== */
+
+/* ========== Public Datatypes ========== */
+
+/* ========== Public Function prototypes ========== */
 
 int main(int argc, char **argv)
 {
+    /**
+     * To identify what functions are used by the standard namespace, we will preface each one with "std::",
+     * instead of implementing the preprocessor directive "using namespace std;"
+     */
     std::cout << "hello, world" << std::endl;
     return EXIT_SUCCESS;
 }
--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # The Basics
-<h2>This is my repository of basic stuff I should remember as a software engineer.</h2>
-<h3>Introduction:</h3>
-<p>My "Common Code" Cheat Sheets</p>
-<p>My System Security Plans</p>
-<p>My Source Control Plan</p>
-<p>My Styling Guide Reference</p>
+<h2>This is my repository of basic stuff I should remember and implement as a software engineer.</h2>
+<p><a href="https://github.com/garciart/Basics/tree/master/CodingTemplates" title="Coding Templates">Coding Templates</a></p>
+<p><a href="https://github.com/garciart/Basics/tree/master/SecurityPlans" title="System Security Plans">System Security Plans</p>
+<p><a href="https://github.com/garciart/Basics/tree/master/SourceControlPlan" title="Source Control Plan">Source Control Plan</p>
+<p><a href="https://github.com/garciart/Basics/tree/master/StylingGuide" title="Styling Guide">Styling Guide</p>
