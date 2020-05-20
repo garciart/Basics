24f8223d09c6cc48db9931024d98151f7cd2712a
garciart <rgarcia@rgprogramming.com>
2020-05-19 23:45:09 -0400

    Formatting date-time for error logging.

--- CPP/hello/models/dev.cpp
+++ CPP/hello/models/dev.cpp
@@ -8,3 +8,27 @@
  * @copyright Copyright (c) 2020
  * 
  */
+
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    char *buffer;
+    printf("%4d-%02d-%02d %02d-%02d-%02d", (1900 + ltm->tm_year), ltm->tm_mon, ltm->tm_mday, ltm->tm_hour, ltm->tm_min, ltm->tm_sec);
+    stringstream fmt;
+    fmt << setw(2) << setfill('0') << (1900 + ltm->tm_year) << "-" << ltm->tm_mon << "-" << ltm->tm_mday << " " << ltm->tm_hour << "-" << ltm->tm_min << "-" << ltm->tm_sec << endl;
+    cout << fmt.str();
+    sprintf(buffer, "%d-%d-%d %d-%d-%d", (1900 + ltm->tm_year), ltm->tm_mon, ltm->tm_mday, ltm->tm_hour, ltm->tm_min, ltm->tm_sec);
+    string formatted_time = buffer;
+    cout << buffer;
+    return EXIT_SUCCESS;
+}
